fcd9ec9baf4f3119d67a923d3b7af25a61f146a2
Voldanis <vovakovinn@yandex.ru>
2026-05-12 12:32:02 +0500

    sucsess test

--- main.py
+++ main.py
@@ -26,4 +26,11 @@ Gallery/fibbig.gif
 #     print(check_colors(picture))
 #print(check_pixels_in_allowed_colors("Gallery/one_bright.png"))
 
+piet.PietInterpreter("Gallery/hw1-11.gif").run()
+piet.PietInterpreter("Gallery/hw3-5.gif").run()
+piet.PietInterpreter("Gallery/pc.png").run()
 piet.PietInterpreter("Gallery/one.png").run()
+print()
+piet.PietInterpreter("Gallery/one_bright.png").run()
+piet.PietInterpreter("Gallery/helloworld-pietbig.gif", 4).run()
+piet.PietInterpreter("Gallery/helloworld-pietbig.gif", 8).run()
