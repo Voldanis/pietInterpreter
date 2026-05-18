806e2d3233ef4f97e70c4aab77d3812b79500cd6
Voldanis <vovakovinn@yandex.ru>
2026-05-18 22:49:58 +0500

    Тестирование

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import piet
 from dop_funcs import *
 from normalizer import Normalizer
 
+'''
 piet.PietInterpreter("Gallery/three.png").run()
 print()
 piet.PietInterpreter("Gallery/hw1-11.gif").run()
@@ -9,31 +10,31 @@ piet.PietInterpreter("Gallery/hw3-5.gif").run()
 piet.PietInterpreter("Gallery/pc.png").run()
 piet.PietInterpreter("Gallery/helloworld-pietbig.gif", 4).run()
 piet.PietInterpreter("Gallery/helloworld-pietbig.gif", 8).run()
-#piet.PietInterpreter("Gallery/one-zigzag.png").run()
 piet.PietInterpreter("Gallery/alpha_filled_big.png").run()
+print()
+piet.PietInterpreter("Gallery/one-zigzag.png").run()
+print()
+piet.PietInterpreter("Gallery/hw2-11.gif").run()  # endless
+piet.PietInterpreter("Gallery/cowsay.png").run()
+piet.PietInterpreter("Gallery/euclid_clint_big.png").run()
+piet.PietInterpreter("Gallery/GameOfLife.png").run()
+piet.PietInterpreter("Gallery/tetris_big.png").run()
+piet.PietInterpreter("Gallery/hw6_big.png").run()
+piet.PietInterpreter("Gallery/ILoveYouLaura.png").run()
+piet.PietInterpreter("Gallery/two_bmp.bmp").run()
+'''
 
 
 
-#print(max_square_size("hw1-11.gif"))
-#print(max_square_size("Gallery/one.png"))
-#print(piet.PietInterpreter.max_square_size("Gallery/pc.png"))
-#piet.PietInterpreter("Gallery/hw1-11.gif").run()
-#piet.PietInterpreter("Gallery/pc.png").run()
-#piet.PietInterpreter("Gallery/one.png").run()
 
-#print(piet.PietInterpreter.max_square_size("Gallery/helloworld-pietbig.gif"))
-#piet.PietInterpreter("Gallery/helloworld-pietbig.gif", 4).run()
 
 '''
 wrong:
-Gallery/cowsay.png
 Gallery/erat2_big.png
-Gallery/euclid_clint_big.png
-Gallery/GameOfLife.png
 Gallery/GetTogetherPiet.png
-Gallery/hw2-11.gif  # белое пространство обрабатывается некоректно... Или нет?
 Gallery/Piet-4.gif  # судя по всему да
 Gallery/fibbig.gif
+Gallery/helloworld-cmb-sm.png
 '''
 # for picture in get_all_filenames("Gallery"):
 #     print(picture)
@@ -62,12 +63,13 @@ Gallery/fibbig.gif
         return 
         
         
+    
+
+bottom = []
+for i in range(shift):
+bottom.append(roll.pop())
+        
         
     
-class WhiteState:
-    def __init__(self, dp, x, y):
-        self.dp = dp
-        self.x = x
-        self.y = y
 
 '''
